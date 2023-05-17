b33cae3c511fc0129f2c6c0958de24e0e4aa8795
Courtney Lippman <courtney.lippman@colorado.edu>
2023-05-17 10:39:37 -0600

    Add logic to save the file to the database.

--- server.js
+++ server.js
@@ -28,8 +28,42 @@ Password: When setting up your database, you might have been prompted to create 
 //Multer storage configuration
 const storage = multer.memoryStorage()
 const upload = multer({ storage })
+// const storage = multer.memoryStorage() creates a new storage engine. memoryStorage engine stores the uploaded file in memory as a 'Buffer' object rather than saving it to disk. It is suitable for smaller files or scenarios where you don't want to persist the uploaded file.
 
 //Express route for file upload
-app.post('/upload', upload.single('image')), (req, res) => {
+app.post('/upload', upload.single('image'), (req, res) => {
     const { originalname, buffer } = req.file
-}
+    
+    // Save the file to the database
+pool.query('INSERT INTO images (name, data) VALUES ($1, $2)', [originalname, buffer], (error, result) => {
+    if(error) {
+        console.error('Error saving image to database', error)
+        res.status(500).send('Error saving image to database')
+    } else {
+        res.send('Image uploaded successfully!')
+    }
+})
+})
+
+//app.post('/upload'....) defines a POST route for the /upload URL path
+
+//upload.single('image') This part is middleware provided by multr. It is used to handle a single upload with the field name 'image'. If the request contains multi files or the field name does not match then 'multr' will rise an error.
+
+// originalname is the name of the uploaded file as specified by the client.
+
+//buffer contains the binary data of the uploaded file stored in memory (if using multer.memoryStorage() as the storage engine)
+
+/*Things to consider with storage:
+1. Disk Storage
+    - multr.diskStorage() to store uploaded files on disk. This approach writes the file to a specified destination on the server's file system.
+    - Configure multer.diskStorage() with a destination directory where the files will be stored. This way, the uploaded image files will be written directly to disk instead of being held in memory.
+    - Disk storage is suitable for larger files as it avoids consuming excessive memory. However, keep in mind that you should have enough disk space available and consider implementing a strategy to manage and clean up old files periodically.
+2. Cloud Storage Services
+    - Instead of storing the files on your local disk, you can integrate multer with cloud storage services like Amazon S3, Google Cloud Storage, Azure Blob Storage.
+    - Using cloud storage services allows you to offload the storage and management of large image files to scalable reliable cloud infrastructure.
+    - Configure multer to use the appropriate storage engine for your chosen cloud storage service. Each service will have its own setup and configuration process. You will typically need to provide authentication credentials, bucket/container names, and other relevant details.
+3. Memory Storage
+    -this is a multer thing that stores and uploads the file in memory
+    - this is great for small, single file uploads. A few megabytes in size or smaller. For example, it works well for handling image thumbnails, small documents, or short audio clips.
+*/
+
